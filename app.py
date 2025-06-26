9f87bc9a6effd5df7c30275c96829ebaf96c3935
Aswanth Vc <aswanth.abam@gmail.com>
2025-06-26 12:00:00 +0530

    feat: add main application and docker scripts

---
+++ app.py
@@ -0,0 +1,96 @@
+from contextlib import asynccontextmanager
+import uuid
+from fastapi import Request
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import ORJSONResponse
+from pydantic import ValidationError
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse
+
+from core.exceptions.core import AbstractException
+from core.response.response_class import CustomORJSONResponse
+from core.loaders.router import autoload_routers
+from core.middlewares.process_time_middleware import ProcessingTimeMiddleware
+from apps.settings import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application Starting Up ...")
+    yield
+    print("application closing")
+
+
+app = FastAPI(lifespan=lifespan, default_response_class=CustomORJSONResponse)
+
+router = autoload_routers("apps")
+
+app.include_router(router=router)
+
+app.add_middleware(ProcessingTimeMiddleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.exception_handler(AbstractException)
+async def abstract_exception_handler(request: Request, exc: AbstractException):
+    return ORJSONResponse(exc.to_json(), status_code=exc.status_code)
+
+
+async def custom_auth_exception_handler(request: Request, exc: Exception):
+    return ORJSONResponse(
+        {
+            "message": "Unauthorized!",
+            "error_code": "UNAUTHORIZED",
+        },
+        status_code=401,
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = []
+    for error in exc.errors():
+        errors.append(
+            {
+                "type": error.get("type", "unknown"),
+                "loc": error.get("loc", []),
+                "msg": error.get("msg", "Invalid input"),
+                "input": error.get("input", None),
+            }
+        )
+    return ORJSONResponse(
+        {
+            "message": "Invalid Request, Please check your request",
+            "error_code": "REQUEST_VALIDATION_ERROR",
+            "errors": errors,
+        },
+        status_code=422,
+    )
+
+
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request: Request, exc: ValidationError):
+    return ORJSONResponse(
+        {
+            "message": "Error Processing Request",
+            "error_code": "VALIDATION_ERROR",
+            "errors": {"msg": f"{e.get('loc')} {e.get('msg')}" for e in exc.errors()},
+        },
+        status_code=500,
+    )
+
+
+app.add_exception_handler(401, custom_auth_exception_handler)
+
+
+@app.get("/api/")
+def root():
+    return HTMLResponse(content="<html><h1>Haa shit! My Code is working.</h1></html>")
