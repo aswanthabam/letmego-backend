46abc65ca56fe9785ac25403f7847d783d961875
Aswanth Vc <aswanth.abam@gmail.com>
2025-06-28 12:00:00 +0530

    feat: add core apps

---
+++ core/exceptions/core.py
@@ -0,0 +1,21 @@
+class AbstractException(Exception):
+    def __init__(
+        self,
+        message: str,
+        err_code: str | None = None,
+        status_code: int = 500,
+        *args,
+        **kwargs
+    ):
+        super().__init__(*args)
+        self.message = message
+        self.error_code = err_code
+        self.kwargs = kwargs
+        self.status_code = status_code
+
+    def to_json(self):
+        return {
+            **self.kwargs,
+            "message": self.message,
+            "error_code": self.error_code,
+        }
